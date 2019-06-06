06d5461b5be3126bfba97b886ac1151be0532a64
j.p.Denoyer <jpdenoyer@gmail.com>
2019-06-06 19:44:04 -0400

    060619 jp 10 lines added each time add button pushed

--- app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -20,6 +20,8 @@ import android.content.ContentValues;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -34,13 +36,69 @@ public class AddTaskActivity extends AppCompatActivity {
     // Declare a member variable to keep track of a task's selected mPriority
     private int mSpeed;
 
+    private int mLinelength = 24;
+
+    InputFilter filter = new InputFilter() {
+        public CharSequence filter(CharSequence source, int start, int end,
+                                   Spanned dest, int dstart, int dend) {
+
+
+            if (start == 0 && end == 0 ) {return null;}
+
+
+            return   source.toString().substring(dend, dend + 1 ).toUpperCase();
+             //return dest.toString().toUpperCase();
+
+
+          //   String sourcestr = source.toString().toUpperCase();
+
+
+
+
+           //  String ret = sourcestr.substring(dend, dend + 1);
+            // if ( sourcestr.length() < mLinelength ) {return  ret.toUpperCase();  }
+
+
+             //return ret;
+
+
+             /*
+
+             int linestart ;
+
+
+
+
+            int j = 0;
+
+            for (int i = start; i < end; i++, j++) {
+                  if (sourcestr.substring(i, i+1).equals("\n"))
+                                {j = 0;}
+
+                  else if (j >=  mLinelength)
+                  {
+                      sourcestr = sourcestr.substring(0, i) + "\n" + sourcestr.substring(i + 1) ;
+                      j = 0;
+                      end = end + 1;
+                  }
+            }
+            return sourcestr;
+*/
+        }
+
+    };
+
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
+      //060619 JP filter doesnt work
+      //  ((EditText) findViewById(R.id.editTextSignData)).setFilters(new InputFilter[] { filter });
 
-        //060319 JP comment this out when replacing radio button with text box
+        //060319 JP comment this out whe01n replacing radio button with text box
         // Initialize to highest mPriority by default (mPriority = 1)
         //((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
 
@@ -49,66 +107,82 @@ public class AddTaskActivity extends AppCompatActivity {
     }
 
 
+
     /**
      * onClickAddTask is called when the "ADD" button is clicked.
      * It retrieves user input and inserts that new task data into the underlying database.
      */
+
     public void onClickAddTask(View view) {
+
+
+        int[] tbid = new int[10];
+
+        tbid[0] = R.id.editTextSignData1;
+        tbid[1] = R.id.editTextSignData2;
+        tbid[2] = R.id.editTextSignData3;
+        tbid[3] = R.id.editTextSignData4;
+        tbid[4] = R.id.editTextSignData5;
+        tbid[5] = R.id.editTextSignData6;
+        tbid[6] = R.id.editTextSignData7;
+        tbid[7] = R.id.editTextSignData8;
+        tbid[8] = R.id.editTextSignData9;
+        tbid[9] = R.id.editTextSignData10;
+
+
         // Not yet implemented
         // Check if EditText is empty, if not retrieve input and store it in a ContentValues object
         // If the EditText input is empty -> don't create an entry
-        String input = ((EditText) findViewById(R.id.editTextSignData)).getText().toString();
-        if (input.length() == 0) {
-            return;
+
+
+        for (int j = 0; j < 10; j++) {
+
+            String input = ((EditText) findViewById(tbid[j])).getText().toString();
+
+            /*
+            if (input.length() == 0) {
+                return;
+            }*/
+
+
+            mSpeed = 3;
+
+            // Insert new task data via a ContentResolver
+            // Create new empty ContentValues object
+            ContentValues contentValues = new ContentValues();
+            // Put the task description and selected mPriority into the ContentValues
+            contentValues.put(TaskContract.TaskEntry.COLUMN_TEXTDATA, input);
+            contentValues.put(TaskContract.TaskEntry.COLUMN_SPEED, mSpeed);
+            // Insert the content values via a ContentResolver
+            Uri uri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, contentValues);
+
+            // Display the URI that's returned with a Toast
+            // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
+            if (uri != null) {
+                Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+            }
+
         }
-
-        try {
-
-            mSpeed =   Integer.parseInt (((EditText) findViewById(R.id.editSpeed)).getText().toString());
-
-        }
-        catch (NumberFormatException e) {
-
-            Log.e("method", "bad speed: " + ((EditText) findViewById(R.id.editSpeed)).getText().toString()) ;
-
-        }
+            // Finish activity (this returns back to MainActivity)
+            finish();
 
 
 
-        // Insert new task data via a ContentResolver
-        // Create new empty ContentValues object
-        ContentValues contentValues = new ContentValues();
-        // Put the task description and selected mPriority into the ContentValues
-        contentValues.put(TaskContract.TaskEntry.COLUMN_TEXTDATA, input);
-        contentValues.put(TaskContract.TaskEntry.COLUMN_SPEED, mSpeed);
-        // Insert the content values via a ContentResolver
-        Uri uri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, contentValues);
 
-        // Display the URI that's returned with a Toast
-        // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
-        if(uri != null) {
-            Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
-        }
+        /**  060319 JP remove function
+         * onPrioritySelected is called whenever a priority button is clicked.
+         * It changes the value of mPriority based on the selected button.
 
-        // Finish activity (this returns back to MainActivity)
-        finish();
-
+         public void onPrioritySelected(View view) {
+         if (((RadioButton) findViewById(R.id.radButton1)).isChecked()) {
+         mPriority = 1;
+         } else if (((RadioButton) findViewById(R.id.radButton2)).isChecked()) {
+         mPriority = 2;
+         } else if (((RadioButton) findViewById(R.id.radButton3)).isChecked()) {
+         mPriority = 3;
+         }
+         }
+         */
     }
-
-
-    /**  060319 JP remove function
-     * onPrioritySelected is called whenever a priority button is clicked.
-     * It changes the value of mPriority based on the selected button.
-
-    public void onPrioritySelected(View view) {
-        if (((RadioButton) findViewById(R.id.radButton1)).isChecked()) {
-            mPriority = 1;
-        } else if (((RadioButton) findViewById(R.id.radButton2)).isChecked()) {
-            mPriority = 2;
-        } else if (((RadioButton) findViewById(R.id.radButton3)).isChecked()) {
-            mPriority = 3;
-        }
-    }
-     */
 
 }
